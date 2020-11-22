b72b715ccfde268a53fd09b4351bd2e8e016e905
Igor Peric <admin@alterigor.ai>
2020-11-22 23:15:27 +0100

    Intent with RootNavigator module

--- App.js
+++ App.js
@@ -7,22 +7,42 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { SongListScreen } from './components/SongListScreen';
 import { SongScreen } from './components/SongScreen';
 import HomeScreen from './components/LogoImage';
-import BottomBarNavigation from './components/BottomBarNavigation'
-import { render } from 'react-dom';
+import BottomBarNavigation from './components/BottomBarNavigation';
+
+import { navigationRef } from './components/NavigationRoot';
+import * as NavigationRoot from './components/NavigationRoot';
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default class App extends Component {
 
+  // navigationRef = React.useRef(null);
+
+  constructor(props) {
+    super(props);
+  }
+
   didPressNavigationButton(index) {
     console.log('BUTTON ' + String(index));
+    if (index == 1) {
+      console.log("WOHOOOOO 1");
+      navigationRef.current?.navigate('SongScreen');
+    } else if (index == 2) {
+      console.log("WOHOOOOO 2");
+      navigationRef.current?.navigate('AddSongScreen');
+    } else if (index == 3) {
+      console.log("WOHOOOOO 3");
+      NavigationRoot.navigate('SongListScreen');
+    } else if (index == 4) {
+        // TODO: add settings screen
+    }
   }
 
   render() {
     return (
       <>
       <NavigationContainer>
-        <Navigator screenOptions={{ headerShown: false }}>
+        <Navigator ref={navigationRef} screenOptions={{ headerShown: false }}>
           {/* <Screen name="HomeScreen" options={{title: 'Home'}} component={HomeScreen}></Screen> */}
           <Screen name="SongListScreen" options={{title: 'Song List'}} component={SongListScreen}></Screen>
           <Screen name="SongScreen" options={{title: 'Chords'}} component={SongScreen}></Screen>
