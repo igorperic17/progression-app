963cab95991e5f19236650c2bfc4d2d63cf6b96a
Igor Peric <admin@alterigor.ai>
2020-11-30 08:41:09 +0100

    Added button styles on Add song screen

--- components/AddSongScreen.js
+++ components/AddSongScreen.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { View, FlatList, Button, StyleSheet, TouchableHighlight, Text, TextInput } from 'react-native';
+import { View, Image, Button, StyleSheet, TouchableHighlight, Text, TextInput } from 'react-native';
 
 import LogoImage from './LogoImage'
 import SongListScreenCell from './SongListScreenCell'
 
 import Song from '../data/Song';
+import { navigationRef } from './NavigationRoot';
+
+import { GlobalColors } from '../styles/global.styles'
+import { LinearGradient } from 'expo-linear-gradient'
 
 class AddSongScreen extends React.Component {
 
-    constructor() {
+    constructor({ navigator }) {
         super();
         this.state = { 
-            song: new Song({id: '1', title: 'test1', artist: 'Igor', chords: 'AMAMAMAM', progression: 'Em,A,C,Hm'})
+            song: new Song({id: '1', title: '-', artist: '-', chords: '-', progression: ''})
         };
         // this.getRemoteData();
+
+        this.navigator = navigator
+    }
+
+    onCancelPress() {
+        console.log("CANCEL PRESSED");
+        this.navigator.goBack();
+    }
+
+    onOKPress() {
+        console.log("OK PRESSED")
     }
     
     render() {
@@ -36,7 +51,26 @@ class AddSongScreen extends React.Component {
 
                 <Text style={styles.inputFieldLabel}>Chords</Text>
                 <TextInput style={[styles.inputField, 
-                    { height: 250, textAlign: 'center', textAlignVertical: 'top'}]}></TextInput>
+                    { height: 250, textAlign: 'center', textAlignVertical: 'top'}]}
+                    onChangeText={ (text) => this.setState({ song: { ...this.state.song, chords: text }})}>
+                        {this.state.song.chords}
+                    </TextInput>
+
+                <View style={styles.buttonRow}>
+                    {/* cancel button */}
+                    <TouchableHighlight style={[ styles.button, {left: 0} ]} onPress={this.onCancelPress}>
+                        <LinearGradient colors={['#EC8D40', '#D5541E']} style={styles.button}>
+                            <Image source={require('../assets/button-x.png')} style={styles.buttonImage}></Image>
+                        </LinearGradient>
+                    </TouchableHighlight>
+
+                    {/* cancel button */}
+                    <TouchableHighlight style={[ styles.button, {right: 0} ]} onPress={this.onOKPress}>
+                        <LinearGradient colors={[GlobalColors.darkBlue, GlobalColors.darkBlue]} style={styles.button}>
+                            <Image source={require('../assets/button-tick.png')} style={styles.buttonImage}></Image>
+                        </LinearGradient>
+                    </TouchableHighlight>
+                </View>
             </View>
             </>
         )
@@ -45,7 +79,7 @@ class AddSongScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        // flex: 1,
+        flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
@@ -67,19 +101,44 @@ const styles = StyleSheet.create({
         // height: 40,
         paddingLeft: 10,
         paddingBottom: 5,
-        marginTop: 15
+        marginTop: 15,
+        color: GlobalColors.lightBlue,
+        fontWeight: 'bold'
     },
     inputField: {
         // flex: 1,
         textAlign: 'center',
         width: '90%',
         height: 60,
-        borderColor: 'black',
+        borderColor: GlobalColors.lightBlue,
         borderRadius: 20,
-        borderWidth: 1,
+        borderWidth: 2,
         marginLeft: 10,
         marginRight: 10,
-        height: 40
+        height: 40,
+        fontWeight: 'bold',
+        color: GlobalColors.darkBlue
+    },
+    buttonRow: {
+        // flex: 1,
+        width: '90%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10
+    },
+    button: {
+        position: 'absolute',
+        top: 0,
+        width: 60,
+        height: 40,
+        borderRadius: 20,
+        // borderWidth: 1,
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    buttonImage: {
+        alignSelf: 'center'
     }
   });
 
---
+++ styles/global.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const GlobalColors = {
+    lightBlue: '#3D79B4',
+    darkBlue: '#0F467C'
+};
+
+export const GlobalStyles = StyleSheet.create({
+    
+});
